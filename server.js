041b9551fd44825842e8e538774a36732a300f37
jp <gaines.johnp@gmail.com>
2017-04-26 15:40:04 -0700

    added view

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const app = express();
+const path = require('path');
 const morgan = require('morgan');
 const serverError = require('debug')('igotgas:error');
 const mongoose = require('mongoose');
@@ -10,11 +11,20 @@ const Promise = require('./public/lib/promise');
 mongoose.Promise = Promise;
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/dev');
 
+//set view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'handlebars');
+
 /*remember to put in routes*/
 
-app.use(morgan('dev')); 
+app.use(morgan('dev'));
+app.use(express.static('./public'));
 
 /*app.use(routes*/
+
+app.get('*', (req, res) => {
+  res.sendfile('./public/views/index.html', { root: '.'});
+});
 
 app.use((err, req, res, next)=> {
   serverError(err);
